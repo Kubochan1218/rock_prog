3507da558824b0f771902218c96f1f0d64b3cc7d
Kubochan1218 <eo23g043@guh.u-hyogo.ac.jp>
2026-06-25 17:37:54 +0900

    config

---
+++ config.py
@@ -0,0 +1,10 @@
+"""定数・設定値をまとめたファイル"""
+
+FILE_PATH = 'attend_data.xlsx'
+SHEET_NAME = '出欠状況'
+FONT_NAME = 'Yu Gothic UI'
+APP_MODE = 'System'  # 'Light' or 'Dark' or 'System'
+APP_COLOR = 'green'  # 'blue' or 'green' or 'dark-blue'
+FONT_TITLE = (FONT_NAME, 20, 'bold')
+FONT_LABEL_BUTTON = (FONT_NAME, 16, 'bold')
+FONT_SUBTITLE = (FONT_NAME, 14)
